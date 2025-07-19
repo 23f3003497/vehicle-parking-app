2d7f79d6da76a8133d1cd3f74f809c1db2557416
23f3003497 <23f3003497@ds.study.iitm.ac.in>
2025-07-19 06:10:15 +0000

    flask setup and login, register end points created

--- application/controllers.py
+++ application/controllers.py
@@ -1,3 +1,39 @@
 from flask import Flask, render_template, redirect, request
 from flask import current_app as app #directly importing app-> circular import error, current_app is the app object created in app.py
-from .models import User, ParkingLot, ParkingSpot, Reserve
+from .models import User, ParkingLot, ParkingSpot, Reserve, db
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username=request.form.get("username")
+        pwd=request.form["pass"]
+        curr_user = User.query.filter_by(username=username).first()
+        if curr_user:
+            if curr_user.password == pwd:
+                if curr_user.type == "admin":
+                    return render_template("admin_dashboard.html", user=curr_user)
+                else:
+                    return render_template("user_dashboard.html", user=curr_user)
+            else:
+                return "Incorrect Password"
+        else:
+            return "User DNE"
+        
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username=request.form.get('username')
+        email=request.form["email"]
+        password=request.form.get("password")
+        address=request.form["address"]
+        pincode=request.form['pincode']
+        if User.query.filter_by(username=username).first():
+            return "Username already exists, try registering with different username"
+        else:
+            new_user=User(username=username, email=email, password=password, address=address, pincode=pincode)
+            db.session.add(new_user)
+            db.session.commit()
+            return "Registered Successfully!"
+    return render_template("register.html")
--- application/models.py
+++ application/models.py
@@ -2,10 +2,12 @@ from .database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), unique=True, nullable=False)
+    username = db.Column(db.String(), nullable=False, unique=True)
+    email = db.Column(db.String(), nullable=False)
     password = db.Column(db.String(), nullable=False)
     type = db.Column(db.String(), default='general')
+    address = db.Column(db.String())
+    pincode = db.Column(db.String())
 
 class ParkingLot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
